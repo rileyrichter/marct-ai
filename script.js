0e3b1800da3412912d1e372aed980327683a5c13
Ben Parker <bparker@gmail.com>
2021-06-25 14:30:05 -0500

    Trying new code to see it this will get the filter working

--- script.js
+++ script.js
@@ -13,29 +13,28 @@ $(".tab-link").click(function () {
 
 // F'in sweet CMS Library for Webflow
 
-// immediately/self invoked function. This function executes right away
 (function () {
-  // create a new Library instance and store it in a variable called "customBlogPosts"
-  var companySubdomain = new FsLibrary(".company-list"); // define 4 filter groups in an array and store it in a variable called myFilters
+  var fsComponent = new FsLibrary(".company-list");
 
   var myFilters = [
     {
-      filterWrapper: ".ai-ml-wrapperfilter", // parent wrapper of filter group1
+      filterWrapper: ".ai-ml-wrapperfilter",
       filterType: "multi",
     },
     {
-      filterWrapper: ".industry-wrapperfilter", // parent wrapper of filter group2
+      filterWrapper: ".industry-wrapperfilter",
       filterType: "multi",
-      filterByClass: ".industry-tag", // class of the text that holds the filter info (optional)
     },
-  ]; // run the filter Library component on your instance
+  ];
 
-  companySubdomain.filter({
-    filterArray: myFilters, // variable of the array we created above
-    activeClass: "ai-ml-active", // class that styles the active state (optional)
-    emptyMessage: ".empty-message",
+  fsComponent.filter({
+    filterArray: myFilters,
+    activeClass: "ai-ml-active",
     animation: {
       enable: true,
+      duration: 300,
+      easing: "ease-in",
+      effects: "fade ",
     },
   });
 })();
