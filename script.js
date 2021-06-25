b8fe9ec8a88447a168f6121174a87c3a1a68a0ed
Ben Parker <bparker@gmail.com>
2021-06-24 20:17:33 -0500

    Updated count to run on Webflow push

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 // Counting the number of items returned after the filter function
-function countItems() {
-  const ul = document.getElementById("company-list");
+// Only run after scripts (and HTML document) are ready
+var Webflow = Webflow || [];
+Webflow.push(function () {
+  // DOMready has fired
+  // May now use jQuery and Webflow api
+  const listItem = document.getElementById("company-list");
   var i = 0,
     itemCount = 0;
-  while (ul.getElementsByClassName("company-card")[i++]) itemCount++;
+  while (listItem.getElementsByClassName("company-card")[i++]) itemCount++;
   document.getElementById("item-count").innerHTML = itemCount;
-}
+});
